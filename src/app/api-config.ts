5195abc7c6da48840b1e0619b572a1d0bbed7020
Patricio Pérez Pinto <patodeath@gmail.com>
2023-02-22 03:07:01 -0300

    refactor: create api-config,ts and fix some css styles

---
+++ src/app/api-config.ts
@@ -0,0 +1,13 @@
+export class ApiConfig {
+
+  ip: string;
+  port: number;
+
+  constructor() {
+    this.ip = "172.17.0.3";
+    this.port = 8080;
+  }
+}
+
+
+
--- src/app/artist/artist.component.ts
+++ src/app/artist/artist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import {ApiConfig} from "../api-config";
 
 @Component({
   selector: 'app-artist',
@@ -11,8 +12,10 @@ export class ArtistComponent implements OnInit {
 
   data: any;
   artist: any;
+  apiConfig: ApiConfig;
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.apiConfig = new ApiConfig();
   }
 
   ngOnInit(): void {
@@ -23,7 +26,7 @@ export class ArtistComponent implements OnInit {
   }
 
   callApiTest(artistId: number): void{
-    this.http.get(`http://172.17.0.4:8080/api/v1/artists/${artistId}`).subscribe(data => {
+    this.http.get(`http://${this.apiConfig.ip}:${this.apiConfig.port}/api/v1/artists/${artistId}`).subscribe(data => {
       this.data = data;
       console.log(this.data);
       this.artist = this.data.artist;
